74ecaa7d1c6ac3d4ebaf306ab7aa3d908d3bb871
LineChen <15764230067@163.com>
2018-11-01 21:35:08 +0800

    过滤器、Django Shell、Admin

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 class Article(models.Model):
     title = models.CharField(max_length=32, default='null')
     content = models.TextField(null=True)
+    pub_time = models.DateTimeField(null=True)
 
     def __str__(self):
         return self.title
